8a30d9bb7fb3adc8e24da14aca853bb84bfbbf70
github username <github email address>
2017-10-02 21:35:44 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  # languages style -> language -> type
+  # rearrangged_languages language -> type, style
+  rearranged_languages = {}
+  
+  languages.each do |style, language|
+		language.each do |name, type|
+			rearranged_languages[name] = {type: type[:type], style: []}
+      
+		end
+	end
+  languages.each {|style, language|
+    rearranged_languages.map {|name, info|
+      if language.include?(name)
+        info[:style] << style
+      end
+    }
+  }
+	rearranged_languages
 end
+
+
